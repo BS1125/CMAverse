005620a015f265ae9f8eef6bae59af0006640aae
BS1125 <sby2018@yeah.net>
2019-07-16 13:55:12 -0400

    .

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,151 @@
-function(s, n) {
-  s$z <- s$estimate / (s$std.error / sqrt(n))
-  s$pval <- 2 * pt(-abs(s$z), n - 1)
-  return(data.frame(z = s$z, pval = s$pval))
+create_formulas <- function(outcome = NULL, treatment = NULL, mediator = NULL, covariates = c(),
+                            interaction = NULL, event = NULL,
+                            mreg = c("linear", "logistic"),
+                            yreg = c("linear", "logistic", "loglinear", "poisson","quasipoisson",
+                                     "negbin", "coxph", "aft_exp", "aft_weibull")) {
+
+
+  mediator_formula_basic <- paste(mediator, treatment, sep = " ~ ")
+
+  outcome_formula_basic  <- paste(paste(outcome, treatment, sep = " ~ "),
+                                  mediator,
+                                  sep = " + ")
+
+  if (interaction) {
+
+    outcome_formula_basic <- paste(outcome_formula_basic,
+                                   paste(treatment, mediator, sep = " * "),
+                                   sep = " + ")
+  }
+
+  if (length(covariates) == 0) {
+
+    mediator_formula <- mediator_formula_basic
+
+    outcome_formula  <- outcome_formula_basic
+
+  } else {
+
+    mediator_formula <- paste(mediator_formula_basic,
+                              paste(covariates, collapse = " + "),
+                              sep = " + ")
+
+    outcome_formula  <- paste(outcome_formula_basic,
+                              paste(covariates, collapse = " + "),
+                              sep = " + ")
+  }
+
+  if (yreg %in% c("coxph","aft_exp","aft_weibull")) {
+
+    l <- strsplit(outcome_formula, split = " ~ ")
+
+    l[[1]][1] <- paste0("Surv(", outcome, ", ", event, ")")
+
+    outcome_formula <- paste(l[[1]][1], l[[1]][2], sep = " ~ ")
+  }
+
+  formulas <- list(outcome_formula = outcome_formula,
+                   mediator_formula = mediator_formula)
+
+  return(formulas)
 }
+
+
+run_regressions <- function(formulas = NULL, outcome, treatment, mediator, covariates,
+                            interaction, event, yreg, mreg, data = NULL) {
+
+  outcome_formula <- formulas$outcome_formula
+
+  mediator_formula<- formulas$mediator_formula
+
+  if (yreg == "linear") {
+    outcome_regression  <- lm(outcome_formula, data = data)
+  }
+
+  if (yreg == "logistic") {
+    outcome_regression  <- glm(outcome_formula, family = binomial(), data = data)
+  }
+
+  if (yreg == "loglinear") {
+    outcome_regression  <- glm(outcome_formula, family = binomial("log"), data = data)
+  }
+
+  if (yreg == "poisson") {
+    outcome_regression  <- glm(outcome_formula, family = poisson(), data = data)
+  }
+
+  if (yreg == "quasipoisson") {
+    outcome_regression  <- glm(outcome_formula, family = quasipoisson(), data = data)
+  }
+
+  if (yreg == "negbin") {
+    outcome_regression  <- glm.nb(outcome_formula, data = data)
+  }
+
+  if (yreg == "coxph") {
+    outcome_regression <- coxph(as.formula(outcome_formula), data = data)
+  }
+
+  if (yreg == "aft_exp") {
+    outcome_regression <- survreg(as.formula(outcome_formula), dist = "exponential", data = data)
+  }
+
+  if (yreg == "aft_weibull") {
+    outcome_regression <- survreg(as.formula(outcome_formula), dist = "weibull", data = data)
+  }
+
+  if (mreg == "linear") {
+    mediator_regression <- lm(mediator_formula, data = data)
+  }
+
+  if (mreg == "logistic") {
+    mediator_regression <- glm(mediator_formula, family = binomial(), data = data)
+  }
+
+  regressions <- list(mediator_regression = mediator_regression,
+                      outcome_regression = outcome_regression)
+
+  return(regressions)
+}
+
+
+get_coef <- function(regressions = NULL, outcome, treatment, mediator, covariates,
+                     interaction, event, mreg, yreg) {
+
+  mediator_regression <- regressions$mediator_regression
+
+  outcome_regression <- regressions$outcome_regression
+
+  ## Store coefficients from regressions
+  betas  <- coefficients(mediator_regression)
+
+  thetas <- coefficients(outcome_regression)
+
+  ## Store covariances from regressions
+  vcov_betas <- vcov(mediator_regression)
+
+  vcov_thetas <- vcov(outcome_regression)
+
+  if (mreg == "linear")
+    variance = summary(mediator_regression)$sigma^2
+  else variance = NULL
+
+  if (yreg == "aft_weibull")
+    vcov_thetas <- vcov_thetas[-nrow(vcov_thetas), -ncol(vcov_thetas)]
+
+  ## Build block diagonal matrix
+  vcov_block <- Matrix::bdiag(vcov_thetas, vcov_betas)
+
+  coef <- list(outcome = outcome, treatment = treatment, mediator = mediator,
+               covariates = covariates, interaction = interaction,
+               event = event, mediator_reg = mreg, outcome_reg = yreg,betas = betas, thetas=thetas,
+               vcov_betas = vcov_betas, vcov_thetas = vcov_thetas, vcov_block = vcov_block,
+               variance = variance)
+
+  return(coef)
+}
+
+
 
 
 format_df_boot <- function(boot.out, conf = 0.95) {
