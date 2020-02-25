5ee415f3d7f2440ff95b1f0457ec1c2d49d4e155
BS1125 <sby2018@yeah.net>
2020-02-25 14:59:44 -0500

    fix bug

--- R/causal_mediation.R
+++ R/causal_mediation.R
@@ -1,7 +1,7 @@
 causal_mediation <- function(data = NULL, outcome = NULL, event = NULL,
                              exposure = NULL, exposure.type = "binary",
                              mediator = NULL, covariates.pre = NULL, covariates.post = NULL,
-                             covariates.post.type = NULL, cval = list(),
+                             covariates.post.type = NULL, cval = NULL,
                              EMint = FALSE, MMint = FALSE, EMMint = FALSE,
                              EMint.terms = NULL, MMint.terms = NULL, EMMint.terms = NULL,
                              mreg = "linear", yreg = "linear",
@@ -32,7 +32,7 @@ causal_mediation <- function(data = NULL, outcome = NULL, event = NULL,
     stop("Unsupported causal mediation model when post-exposure covariates exist.")
   }
 
-##########################################reorganize the dataset################################
+  ##########################################reorganize the dataset################################
 
   if (exposure.type == "binary") {
     data[, exposure] <- as.factor(data[, exposure])
@@ -74,43 +74,46 @@ causal_mediation <- function(data = NULL, outcome = NULL, event = NULL,
     }
   }
 
-if (model != "ne") {
+  if (model != "ne") {
 
-  if (model == "rb" && est.method == "paramfunc") {
+    if (model == "rb" && est.method == "paramfunc") {
 
-    if (is.null(covariates.pre)) {
-      vecc <- NULL
-    } else {
+      if (is.null(covariates.pre)) {
+        vecc <- NULL
+      } else if (!is.null(covariates.pre)) {
 
-      if ((length(covariates.pre) != length(cval))) {
+        vecc <- c()
 
-        stop("length(covariates.pre) != length(cval), values of pre-exposure covariates needed")
+        covariates.pre.new <- c()
 
-      }
+        if (is.null(cval)) cval <- as.list(rep(NA, length(covariates.pre)))
 
-      vecc <- c()
+        for (i in 1:length(covariates.pre)) {
 
-      covariates.pre.new <- c()
+          if (is.numeric(data[, covariates.pre[i]])) {
 
-      for (i in 1:length(covariates.pre)) {
+            if (!is.null(cval[[i]])&&!is.na(cval[[i]])){vecc <- c(vecc, cval[[i]])
+            } else  {vecc <- c(vecc, mean(data[, covariates.pre[i]]))}
 
-        if (is.numeric(data[, covariates.pre[i]])) {
-          vecc <- c(vecc, cval[[i]])
-        } else if (!is.numeric(data[, covariates.pre[i]])) {
-          data[,covariates.pre[i]] <- as.factor(data[,covariates.pre[i]])
-          mid <- which(levels(data[,covariates.pre[i]]) == cval[[i]])
-          levels(data[,covariates.pre[i]]) <- 0:(length(levels(data[,covariates.pre[i]]))-1)
-          vecc <- c(vecc, mid - 1)
+          } else if (!is.numeric(data[, covariates.pre[i]])) {
+            data[,covariates.pre[i]] <- as.factor(data[,covariates.pre[i]])
+            mid <- which(levels(data[,covariates.pre[i]]) == cval[[i]])
+            levels(data[,covariates.pre[i]]) <- 0:(length(levels(data[,covariates.pre[i]]))-1)
+
+            if (!is.null(cval[[i]])&&!is.na(cval[[i]])) {
+              vecc <- c(vecc, as.numeric(levels(data[,covariates.pre[i]]) == mid - 1)[-1])
+            } else  {vecc <- c(vecc, unname(colMeans(model.matrix(as.formula(paste0("~", covariates.pre[i])), data=data)[,-1])))}
+
+          }
         }
       }
-
     }
 
-  }
 
   n <- nrow(data)
 
-  effect_estimate <- est_step(data = data, indices = 1:n, outcome = outcome, exposure = exposure,
+  effect_estimate <- est_step(data = data,
+                              outcome = outcome, exposure = exposure,
                               exposure.type = exposure.type, mediator = mediator,
                               covariates.pre = covariates.pre, covariates.post = covariates.post,
                               covariates.post.type = covariates.post.type, vecc = vecc,
@@ -133,62 +136,62 @@ if (model != "ne") {
   out <- list(effect_estimate = effect_estimate, effect_se = effect_se)
 
 
-  } else if (model == "ne") {
+} else if (model == "ne") {
 
-    formulas <- create_formulas(outcome=outcome, exposure=exposure, mediator=mediator,
-                             covariates.pre=covariates.pre, covariates.post=covariates.post,
-                             EMint=EMint, MMint=MMint, EMMint=EMMint,
-                             EMint.terms=EMint.terms, MMint.terms=MMint.terms, EMMint.terms=EMMint.terms,
-                             event=event, mreg=mreg, yreg=yreg, model=model)
+  formulas <- create_formulas(outcome=outcome, exposure=exposure, mediator=mediator,
+                              covariates.pre=covariates.pre, covariates.post=covariates.post,
+                              EMint=EMint, MMint=MMint, EMMint=EMMint,
+                              EMint.terms=EMint.terms, MMint.terms=MMint.terms, EMMint.terms=EMMint.terms,
+                              event=event, mreg=mreg, yreg=yreg, model=model)
 
-    regressions <- run_regressions(model = model, formulas = formulas,
-                                exposure = exposure, exposure.type = exposure.type,
-                                mediator = mediator, covariates.post = covariates.post,
-                                covariates.post.type = covariates.post.type,
-                                mreg = mreg, yreg = yreg, data = data)
+  regressions <- run_regressions(model = model, formulas = formulas,
+                                 exposure = exposure, exposure.type = exposure.type,
+                                 mediator = mediator, covariates.post = covariates.post,
+                                 covariates.post.type = covariates.post.type,
+                                 mreg = mreg, yreg = yreg, data = data)
 
-    expData <- medflex::neImpute(regressions$outcome_regression, data = data,
-                                 nMed = length(mediator), nrep = nrep)
+  expData <- medflex::neImpute(regressions$outcome_regression, data = data,
+                               nMed = length(mediator), nrep = nrep)
 
-    if (EMint == TRUE) {
+  if (EMint == TRUE) {
 
-      medflex_formula <- paste(c(paste(outcome, paste0(exposure, "0"), sep = "~"), paste0(exposure, "1"),
-                                 paste(paste0(exposure, "0"), paste0(exposure, "1"), sep = "*"), covariates.pre),
-                               collapse = "+")
+    medflex_formula <- paste(c(paste(outcome, paste0(exposure, "0"), sep = "~"), paste0(exposure, "1"),
+                               paste(paste0(exposure, "0"), paste0(exposure, "1"), sep = "*"), covariates.pre),
+                             collapse = "+")
 
-    } else if (EMint == FALSE) {
+  } else if (EMint == FALSE) {
 
-      medflex_formula <- paste(c(paste(outcome, paste0(exposure, "0"), sep = "~"), paste0(exposure, "1"),
-                                 covariates.pre),
-                               collapse = "+")
-
-    }
-
-    assign("medflex_formula", medflex_formula, envir = globalenv())
-
-    family <- regressions$outcome_regression$family
-
-    out <- summary(medflex::neEffdecomp(medflex::neModel(as.formula(medflex_formula),
-                                                         family = family,
-                                                         expData = expData, se = "robust")))$coefficients[, c("Estimate", "Std. Error")]
-
-    if (yreg != "linear") {
-
-      out[, "Estimate"] <- exp(out[, "Estimate"])
-
-      for (i in 1:length(out[, "Std. Error"]))
-        out[, "Std. Error"][i] <- msm::deltamethod(as.formula("~exp(x1)"), out[, "Estimate"][i],
-                                                   out[, "Std. Error"][i])
-
-    }
-
-    rm(medflex_formula, pos = ".GlobalEnv")
+    medflex_formula <- paste(c(paste(outcome, paste0(exposure, "0"), sep = "~"), paste0(exposure, "1"),
+                               covariates.pre),
+                             collapse = "+")
 
   }
 
+  assign("medflex_formula", medflex_formula, envir = globalenv())
 
+  family <- regressions$outcome_regression$family
 
-  return(out)
+  out <- summary(medflex::neEffdecomp(medflex::neModel(as.formula(medflex_formula),
+                                                       family = family,
+                                                       expData = expData, se = "robust")))$coefficients[, c("Estimate", "Std. Error")]
+
+  if (yreg != "linear") {
+
+    out[, "Estimate"] <- exp(out[, "Estimate"])
+
+    for (i in 1:length(out[, "Std. Error"]))
+      out[, "Std. Error"][i] <- msm::deltamethod(as.formula("~exp(x1)"), out[, "Estimate"][i],
+                                                 out[, "Std. Error"][i])
 
   }
+
+  rm(medflex_formula, pos = ".GlobalEnv")
+
+}
+
+
+
+return(out)
+
+}
 
