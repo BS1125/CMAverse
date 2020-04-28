889b599dc31bdb2de2d58f5c501eda01a4625647
BS1125 <sby2018@yeah.net>
2020-04-28 06:39:42 -0400

    cmdag modification

--- R/cmdag.R
+++ R/cmdag.R
@@ -1,11 +1,11 @@
 cmdag <- function(outcome = NULL, exposure = NULL, mediator = NULL,
-                  covariates.pre = NULL, covariates.post = NULL) {
+                  prec = NULL, postc = NULL) {
 
   require(ggdag)
   require(gridExtra)
   require(grid)
 
-  if (is.null(covariates.post)) {
+  if (is.null(postc)) {
 
   dag <- dagify(Y~A+M+C,M~A+C,A~C,coords=list(x=c(A=0,Y=4,M=2,C=2),y=c(A=0,Y=0,M=1,C=2)))
 
@@ -22,7 +22,7 @@ cmdag <- function(outcome = NULL, exposure = NULL, mediator = NULL,
                  bottom = textGrob(paste0("A(Exposure):", exposure, "\n M(Mediator):",
                                           paste(mediator, collapse = ", "), "\n Y(Outcome):",
                                           outcome, "\n C(Pre-exposure confounders) \n L(Post-exposure confounders):",
-                                          paste(covariates.post, collapse = ", ")),
+                                          paste(postc, collapse = ", ")),
                                    x = 1,
                                    hjust = 1, gp = gpar(fontface = 3L, fontsize = 8)))
 
