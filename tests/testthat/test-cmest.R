8b0175dbd0bbeb355c478c10eb37562f797d97d1
BS1125 <sby2018@yeah.net>
2020-11-02 17:05:03 -0500

    fix tests

--- tests/testthat/test-cmest.R
+++ tests/testthat/test-cmest.R
@@ -244,6 +244,91 @@ test_that("cmest works correctly for continuous Y and binary M", {
   
 })
 
+
+test_that("cmest works correctly for bca", {
+  
+  set.seed(1)
+  # data simulation
+  expit <- function(x) exp(x)/(1+exp(x))
+  n <- 10000
+  C1 <- rnorm(n, mean = 1, sd = 1)
+  C2 <- rbinom(n, 1, 0.6)
+  pa <- expit(0.2 + 0.5*C1 + 0.1*C2)
+  A <- rbinom(n, 1, pa)
+  pm <- expit(1 + 2*A + 1.5*C1 + 0.8*C2)
+  M <- rbinom(n, 1, pm)
+  Y <- rnorm(n, -1 + 0.8*A + 0.5*M + 0.5*A*M + 0.3*C1 - 0.6*C2, 1)
+  data <- data.frame(A, M, Y, C1, C2)
+  
+  # results of cmest
+  res_contbin_rb_param_bootstrap <- cmest(data = data, model = "rb", outcome = "Y", exposure = "A",
+                                          mediator = "M", basec = c("C1", "C2"), EMint = TRUE,
+                                          mreg = list("logistic"), yreg = "linear",
+                                          astar = 0, a = 1, mval = list(1),
+                                          estimation = "paramfunc", inference = "bootstrap",
+                                          boot.ci.type = "bca")
+  res_contbin_rb_impu_bootstrap <- cmest(data = data, model = "rb", outcome = "Y", exposure = "A",
+                                         mediator = "M", basec = c("C1", "C2"), EMint = TRUE,
+                                         mreg = list("logistic"), yreg = "linear",
+                                         astar = 0, a = 1, mval = list(1),
+                                         estimation = "imputation", inference = "bootstrap",
+                                         boot.ci.type = "bca")
+  res_contbin_wb <- cmest(data = data, model = "wb", outcome = "Y", exposure = "A",
+                          mediator = "M", basec = c("C1", "C2"), EMint = TRUE,
+                          ereg = "logistic", yreg = "linear",
+                          astar = 0, a = 1, mval = list(1),
+                          estimation = "imputation", inference = "bootstrap",
+                          boot.ci.type = "bca")
+  res_contbin_iorw <- cmest(data = data, model = "iorw", outcome = "Y", exposure = "A",
+                            mediator = "M", basec = c("C1", "C2"), EMint = TRUE,
+                            ereg = "logistic", yreg = "linear",
+                            astar = 0, a = 1, mval = list(1),
+                            estimation = "imputation", inference = "bootstrap",
+                            boot.ci.type = "bca")
+  res_contbin_msm <- cmest(data = data, model = "msm", outcome = "Y", exposure = "A",
+                           mediator = "M", basec = c("C1", "C2"), EMint = TRUE,
+                           ereg = "logistic", yreg = "linear", mreg = list("logistic"),
+                           wmnomreg = list("logistic"), wmdenomreg = list("logistic"),
+                           astar = 0, a = 1, mval = list(1),
+                           estimation = "imputation", inference = "bootstrap",
+                           boot.ci.type = "bca")
+  res_contbin_ne <- cmest(data = data, model = "ne", outcome = "Y", exposure = "A",
+                          mediator = "M", basec = c("C1", "C2"), EMint = TRUE,
+                          yreg = "linear",
+                          astar = 0, a = 1, mval = list(1),
+                          estimation = "imputation", inference = "bootstrap",
+                          boot.ci.type = "bca")
+  res_contbin_gformula <- cmest(data = data, model = "gformula", outcome = "Y", exposure = "A",
+                                mediator = "M", basec = c("C1", "C2"), EMint = TRUE,
+                                mreg = list("logistic"), yreg = "linear",
+                                astar = 0, a = 1, mval = list(1),
+                                estimation = "imputation", inference = "bootstrap",
+                                boot.ci.type = "bca")
+  
+  # test
+  ref <- summary(res_contbin_rb_param_bootstrap)$summarydf$Estimate
+  expect_equal(summary(res_contbin_rb_impu_bootstrap)$summarydf$Estimate, ref, tolerance = 0.1)
+  expect_equal(summary(res_contbin_wb)$summarydf$Estimate, ref, tolerance = 0.1)
+  expect_equal(summary(res_contbin_msm)$summarydf$Estimate, ref, tolerance = 0.1)
+  expect_equal(summary(res_contbin_ne)$summarydf$Estimate, ref, tolerance = 0.1)
+  expect_equal(summary(res_contbin_iorw)$summarydf$Estimate, ref[c(6,2,5,15)], tolerance = 0.1)
+  expect_equal(summary(res_contbin_gformula)$summarydf$Estimate, ref, tolerance = 0.1)
+  expect_equal(class(print(res_contbin_rb_param_bootstrap)), "list")
+  expect_equal(class(print(res_contbin_rb_impu_bootstrap)), "list")
+  expect_equal(class(print(res_contbin_wb)), "list")
+  expect_equal(class(print(res_contbin_msm)), "list")
+  expect_equal(class(print(res_contbin_ne)), "list")
+  expect_equal(class(print(res_contbin_iorw)), "list")
+  expect_equal(class(print(res_contbin_gformula)), "list")
+  expect_equal(class(print(summary(res_contbin_rb_impu_bootstrap))), "list")
+  expect_equal(class(print(summary(res_contbin_wb))), "list")
+  expect_equal(class(print(summary(res_contbin_msm))), "list")
+  expect_equal(class(print(summary(res_contbin_ne))), "list")
+  expect_equal(class(print(summary(res_contbin_iorw))), "list")
+  expect_equal(class(print(summary(res_contbin_gformula))), "list")
+})
+
+
 test_that("cmest works correctly for survival Y and count M", {
   
   set.seed(1)
@@ -573,3 +658,4 @@ test_that("cmest works correctly for continuous Y and gamma M", {
                unname(res_gammalinear_gformula$effect.pe), tolerance = 0.1)
   
 })
+
